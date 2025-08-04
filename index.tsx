6fd76a0c9dbcee7219faff74ec0050ed58c0bac1
JoseLuis <joseluishernandeztareas@gmail.com>
2025-08-04 15:50:07 -0700

    index

--- index.tsx
+++ index.tsx
@@ -1,3 +1,17 @@
+// @ts-nocheck
+window.addEventListener('error', e => {
+  if (e.message === 'ResizeObserver loop limit exceeded' || e.message === 'ResizeObserver loop completed with undelivered notifications.') {
+    e.stopImmediatePropagation();
+  }
+});
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (typeof args[0] === 'string' && /Could not establish connection/.test(args[0])) {
+    return;
+  }
+  originalError.apply(console, args);
+};
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
